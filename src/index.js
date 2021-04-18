900df5a14ded97952d9e4d4af2f78e4d3c724d1e
Juan Ramon Bojorquez <juanito@Juan-Ramon-Bojorquezs-MacBook-Pro.local>
2021-04-17 18:52:00 -0700

    editios

--- src/index.js
+++ src/index.js
@@ -13,18 +13,3 @@ ReactDOM.render(
   </StrictMode>,
   rootElement
 );
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-
-import "./App.css";
-
-import App from "./App";
-import Weather from "./Weather";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
